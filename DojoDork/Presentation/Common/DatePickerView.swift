582b6f2818bcbbad41e1e3b0d9e62ecc69990422
Eric Ziegler <eric@zigamajig.com>
2025-04-20 16:07:25 -0400

    EZ: Implement DatePickerView and LoadingView.

--- DojoDork/Presentation/Common/DatePickerView.swift
+++ DojoDork/Presentation/Common/DatePickerView.swift
@@ -5,9 +5,45 @@
 import SwiftUI
 
 struct DatePickerView: View {
+    @Environment(\.dismiss) private var dismiss
+    @State private var selectedDate: Date = Date()
+    
+    var minDate: Date = Date.distantPast
+    var maxDate: Date = Date.distantFuture
+    var onDateSelected: ((Date) -> Void)?
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                DatePicker(
+                    "Select a date",
+                    selection: $selectedDate,
+                    in: minDate...maxDate,
+                    displayedComponents: [.date]
+                )
+                .datePickerStyle(GraphicalDatePickerStyle())
+                .padding()
+                .onChange(of: selectedDate) { _, newDate in
+                    onDateSelected?(newDate)
+                    dismiss()
+                }
+                Spacer()
+            }
+            .navigationTitle("Pick a Date")
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("Cancel")
+                            .appBodyMediumStyle()
+                    }
+
+                }
+            }
+        }
+        .preferredColorScheme(.dark)
+        .tint(.brand)
     }
 }
 
--- DojoDork/Presentation/Common/LoadingView.swift
+++ DojoDork/Presentation/Common/LoadingView.swift
@@ -2,3 +2,46 @@
 // Created by Eric Ziegler on 4/20/25
 //  
 
+import SwiftUI
+import SwiftfulLoadingIndicators
+
+struct LoadingView: View {
+    
+    var text: String?
+    
+    var body: some View {
+        ZStack {
+            renderBackground()
+            renderLoadingIndicator()
+        }
+        .ignoresSafeArea()
+    }
+    
+    @ViewBuilder private func renderBackground() -> some View {
+        Rectangle()
+            .fill(.black.opacity(0.8))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+    
+    @ViewBuilder private func renderLoadingIndicator() -> some View {
+        VStack(spacing: 28) {
+            LoadingIndicator(animation: .threeBallsTriangle, color: .brand, size: .medium)
+            if let text {
+                Text(text)
+                    .appSubtitleStyle()
+                    .foregroundStyle(.appText)
+            }
+        }
+    }
+}
+
+#Preview {
+    ZStack {
+        SolidBackground {
+            Text("Testing")
+                .appSubtitleStyle()
+                .foregroundStyle(.appText)
+        }
+        LoadingView(text: "Loading...")
+    }
+}
